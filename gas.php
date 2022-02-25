7c7af9e858bcc9e7981192f78e269ab7c6b32f86
Arystannn <95416145+Arystannn@users.noreply.github.com>
2022-02-25 15:37:56 +0600

    Add files via upload

--- gas.php
+++ gas.php
@@ -6,15 +6,15 @@ $fio = $_POST['FIO'];
 
 require __DIR__ . '/vendor/autoload.php';
 $client = new \Google_Client();
-$client->setApplicationName('')
+// $client->setApplicationName('test')
 $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
 $client->setAuthConfig(__DIR__ . '/credentials.json');
 $service = new Google_Service_Sheets($client);
 $spreadsheetId = "1Z0fsErosB3fFhZvmUj82KDFtEkR07rZMa3XJ3fkB7E4";
 
-$range = "БД";
+$range = "Data";
 $values = [
-    ['', $_POST['mail'],$_POST['tel'];$_POST['FIO'];
+    [ $_POST['mail'],$_POST['tel'];$_POST['FIO'];
 ]
 $body = new Google_Service_Sheets_ValueRange([
     'values' => $values
